a7992bff8ed441aa33f45daa3c68d854296a739e
Admin <lmht721@gmail.com>
2024-06-17 09:28:30 +0700

    add bt

---
+++ index.js
@@ -0,0 +1,24 @@
+let x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let first = 0;
+let last = x.length - 1;
+while (first < last) {
+  let b = x[first];
+  x[first] = x[last];
+  x[last] = b;
+  first++;
+  last--;
+}
+document.getElementById("result").innerHTML = x;
+
+
+// let x = [-3, 5, 1, 3, 2, 10];
+// let first = 0;
+// let last = x.length - 1;
+// while (first < last) {
+//     let b = x[first];
+//     x[first] = x[last];
+//     x[last] = b;
+//     first++;
+//     last--;
+// }
+// console.log(x);
